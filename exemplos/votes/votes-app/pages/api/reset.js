0ffa53c848b929858388fa99e7f5285fbe6b435d
Fabricio Bizotto <fabricio.bizotto@ifc.edu.br>
2024-01-29 19:10:46 -0300

    asd

--- exemplos/votes/votes-app/pages/api/reset.js
+++
@@ -1,7 +0,0 @@
-// voting-app/pages/api/reset.js
-import { resetVotes } from '../../lib/votes';
-
-export default function handler(req, res) {
-  resetVotes();
-  res.status(200).json({ message: 'Votes reset successfully' });
-}
